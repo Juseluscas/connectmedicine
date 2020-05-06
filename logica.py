b83b29d18c5378e1e65f7a8f75ee17285b5a67d8
Juseluscas <jos3.lucasjlp@gmail.com>
2020-05-06 10:48:51 -0300

    aquele não foi o ultimo, esse é

--- logica.py
+++ logica.py
@@ -131,7 +131,7 @@ def logica(comando,usermail):
             msg="\n A(s) palavra(s) que você utilizou chegaram perto da palavra chave **paciente**, tente ela"
             return msg, arquivo
         elif box2 == "help" or box2 == "connected medicine help" or box2 == "connected help":
-            msg="\n Para entender o meu funcionamento é fácil, temos algumas palavras grafas em **negrito**, utilize essas palavras para me responder, assim fica fácilpara mim procurar uma resposta para você ;)"
+            msg="\n Para entender o meu funcionamento é fácil, temos algumas palavras grafadas em **negrito**, utilize essas palavras para me responder, assim fica fácil para mim procurar uma resposta para você ;)"
             return msg, arquivo 
 
         else:
